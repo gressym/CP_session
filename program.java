c49ff1215f87b0b0b1c6af88bd227b4dc3e8b3e5
GressyMahadule <140788167+gressym@users.noreply.github.com>
2024-02-16 10:09:50 +0000

    committed

---
+++ program.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+import java.io.*;
+public class program
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc= new Scanner(System.in);
+		System.out.println("Enter any character:");
+		char ch=sc.next().charAt(0);
+		if(ch>='a' && ch<='z')
+		{
+		    
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+		{
+		    System.out.println("Vowel");
+		    
+		}
+		else 
+		{
+		    System.out.println("Consonant");
+		    
+		}
+		}
+		else if(ch>='A' && ch<='Z')
+		{
+		    if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+		{
+		    System.out.println("Vowel");
+		    
+		}
+		else 
+		{
+		    System.out.println("Consonant");
+		    
+		}
+		}
+		else
+		{
+		    System.out.println("Invalid input.");
+		}
+	}
+}
